3f6416237ca995dc8c5acb8802da10e8c45b9c6e
davorkovacic <80972996+davorkovacic@users.noreply.github.com>
2022-05-18 11:29:07 +0200

    Update UPN_Projekt.ino

--- UPN_Projekt.ino
+++ UPN_Projekt.ino
@@ -4,7 +4,9 @@ int b3 = 6;
 int b4 = 7;
 int LED1 = 8;
 int LED2 = 9;
-void setup() {
+int Led1Stanje = 0;
+int Led2Stanje = 0;
+void setup(){
 pinMode(LED1,OUTPUT);
 pinMode(LED2,OUTPUT);
 pinMode(b1,INPUT);
@@ -13,25 +15,22 @@ pinMode(b3,INPUT);
 pinMode(b4,INPUT);  
 }
 void loop() {
-  if (digitalRead(b1)==1){
-      digitalWrite(LED1,0);
-      digitalWrite(LED2,1);
+  if (digitalRead(b1)){
+    Led1Stanje=(Led1Stanje+1)%2;
       }
-  if (digitalRead(b2) == 1){
-    digitalWrite(LED2,0);
-    digitalWrite(LED1,1);
+  if (digitalRead(b2)){
+    Led2Stanje=(Led2Stanje+1)%2;
    }
-  if(digitalRead(b3)== 1){
-    digitalWrite(LED2,0);
-    digitalWrite(LED1,0);
+  if(digitalRead(b3)){
+    Led1Stanje=0;
+    Led2Stanje=0;
   }
-  if(digitalRead(b4)== 1){
-    if(digitalRead(LED1)==1){
-      digitalWrite(LED1,0);
-      digitalWrite(LED2,1);
-    }else{
-      digitalWrite(LED1,1);
-      digitalWrite(LED2,0);
+  if(digitalRead(b4)){
+    if (Led1Stanje != Led2Stanje){
+      Led1Stanje=(Led1Stanje+1)%2;
+      Led2Stanje=(Led2Stanje+1)%2;
     }
-  }
-}
+    }
+  digitalWrite(LED1,Led1Stanje);
+  digitalWrite(LED2,Led2Stanje);  
+ }
